de4e7ddf0072c69e6520c48d2de127da73b5d4c6
Victor <viktors@cwise.eu>
2025-07-08 10:50:57 +0300

    update fixed tf

--- main.tf
+++ main.tf
@@ -42,12 +42,10 @@ resource "helm_release" "kubernetes-external-secrets" {
   wait_for_jobs = true
   force_update  = false
   recreate_pods = true
-  set = [
-    {
+  set {
       name  = "installCRDs"
       value = "true"
-    },
-  ]
+    }
   namespace = kubernetes_namespace.kubernetes-external-secrets.metadata.0.name
   values = [
     templatefile("${path.module}/templates/external-secrets.yaml.tpl",
